199d21837275e06c41a41c4f1648cce8fb3fe758
Oleg Stahniuk <oleh.stahnyuk@gmail.com>
2020-05-30 17:40:46 +0300

    ending burger menu

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 (function(){
 	const burger2 = document.querySelector('.header-menu')
+	const burger4 = document.querySelector('.burger__nav')
 	burger2.addEventListener('click', () =>{
 		burger2.classList.toggle('menu-active')
+		burger4.classList.toggle('burger__nav-active')
 	})
 }())
